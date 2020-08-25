d9a3d226dbb6c6f8fd7864ca4fda60ba09fb14c4
Linda Eng <90lineng@gmail.com>
2020-08-25 13:47:17 -0400

    feat: added iframe

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import VideoItem from './VideoItem'
 
-const VideoList = ({videos}) => {
+const VideoList = ({videos, onVideoSelect}) => {
   const renderedList = videos.map((video) => {
     console.log('VIDEOOO',video.snippet.channelTitle)
-     return <VideoItem video={video}/>
+     return <VideoItem onVideoSelect={onVideoSelect} video={video}/>
   })
 
 return <div className= "ui relaxed divided list">{renderedList}</div>
